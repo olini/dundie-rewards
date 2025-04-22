a4c2b700788f36c3364d03a51c45609dc90b4a5b
root <root@DESKTOP-PIJ9BTE.>
2025-04-22 14:20:31 -0300

    add main files for package build and install. add docs folder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point System for Dunder Mifflin",
+    author="Lucas Olini",
+    packages=find_packages(),  # packages=["dundie"],  # podemos especificar as pastas que contem os pacotes
+)
